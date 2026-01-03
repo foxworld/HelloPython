6bb16d11bf78b43789eaa87673625e06c385c691
foxworld <foxworld@gmail.com>
2026-01-03 18:05:28 +0900

    study

---
+++ python_advanced/p_chapter03_01.py
@@ -0,0 +1,19 @@
+# Chapter03-02
+# Special Method(Magic Method)
+# 파이썬의 핵심 -. 시퀀스(Sequence),반복(Iterator), 함수(Functions),  클래스(Class)
+# 클래스안에 정의할 수 있는 특별한(Built-in) 메소드
+
+# 기본형
+print(int)
+print(float)
+
+# 모든속성 및 메스드 출력
+print(dir(int))
+print(dir(float))
+
+n = 10
+# print(type(n))
+print(n + 100)
+print(n.__add__(100))
+print(n.__doc__)
+print(n.__bool__(), bool(n))
