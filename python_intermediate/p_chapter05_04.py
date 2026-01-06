c00ea1bc951ccc8bc08e9b6ace45479ad9c2b8c4
이훈구 <foxworld@gmail.com>
2026-01-06 15:01:54 +0900

    study

--- python_intermediate/p_chapter05_04.py
+++ python_intermediate/p_chapter05_04.py
@@ -13,4 +13,57 @@
 # 2. 특정 기능에 한정된 함수는 -> 단일 함수로 작성하는 것이 유리
 # 3. 디버깅 불편
 
-# 데이터 실습
+# 데코레이터 실습
+import time
+
+def perf_clock(func):
+
+    def pref_clocked(*args):
+        # 함수 시작시간
+        st = time.perf_counter()
+        #  함수 실행
+        result = func(*args)
+        # 함수종료시간
+        et = time.perf_counter() - st
+        # 실행 함수명
+        name = func.__name__
+        # 함수 매개변수
+        arg_str = ', '.join(repr(arg) for arg in args)
+        # 결과 출력
+        print('[%0.5fs] %s(%s) -> %r' % (et, name, arg_str, result))
+
+        return result
+
+    return pref_clocked
+
+@perf_clock
+def time_func(seconds):
+    time.sleep(seconds)
+
+@perf_clock
+def sum_func(*numbers):
+    return sum(numbers)
+
+# 데코레이터 미사용
+none_deco1 = perf_clock(time_func)
+none_deco2 = perf_clock(sum_func)
+
+print(none_deco1, none_deco1.__code__.co_freevars)
+print(none_deco2, none_deco2.__code__.co_freevars)
+print('-' * 40, 'Called None Decorator -> time_func')
+none_deco1(1.5)
+print()
+print('-' * 40, 'Called None Decorator -> sum_func')
+none_deco2(100, 200, 300, 400, 500)
+print()
+print()
+
+# 데코레이터 사용
+print('-' * 40, 'Called Decorator -> time_func')
+time_func(1.5)
+print()
+print('-' * 40, 'Called Decorator -> sum_func')
+sum_func(100, 200, 300, 400, 500)
+print()
+print()
+
