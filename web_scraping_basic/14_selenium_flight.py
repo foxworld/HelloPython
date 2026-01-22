c05e1e1342216db228da02adb303ca95b14ffa1d
이훈구 <foxworld@gmail.com>
2026-01-22 15:34:46 +0900

    web scraping study

--- web_scraping_basic/14_selenium_flight.py
+++ web_scraping_basic/14_selenium_flight.py
@@ -8,13 +8,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 browser.maximize_window() # 창 최대화
 
+def get_element(elems, target):
+    for elem in elems:
+        if elem.find_element(By.TAG_NAME, "B").text.strip() == target:
+            break
+    return elem
+
 url = "https://flight.naver.com/flights/"
 browser.get(url)
-browser.find_element(By.XPATH, "//*[@id='__next']/div/main/div[3]/div/div/div[2]/div[2]/button[1]").click()
+browser.find_element(By.CSS_SELECTOR, '[data-event-area-code="rnd.depdate1"]').click()
+time.sleep(5)
 
-# 이번달 27일 28일 선택
-browser.find_elements(By.LINK_TEXT, "27")[0].click()
-browser.find_elements(By.LINK_TEXT, "28")[0].click()
+elems = browser.find_elements(By.CSS_SELECTOR, ".sc-jlZhew.hTjJbq.inner")
+elem = get_element(elems, "26")
+print(elem.text)
+elem.click()
 
+elems = browser.find_elements(By.CSS_SELECTOR, ".sc-jlZhew.hTjJbq.inner")
+elem = get_element(elems, "31")
+print(elem.text)
+elem.click()
 
 time.sleep(100)
