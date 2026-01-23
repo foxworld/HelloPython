58afde72c9d4456c5d2eab70fa0033340fbfb7cb
이훈구 <foxworld@gmail.com>
2026-01-23 16:18:19 +0900

    web scraping study

--- web_scraping_basic/15_selenium_books.py
+++ web_scraping_basic/15_selenium_books.py
@@ -13,9 +13,15 @@ soup = BeautifulSoup(res.text, 'lxml')
 books = soup.find_all("div", attrs={"class": "VfPpkd-EScbFb-JIbuQc UVEnyf"})
 print(len(books))
 for book in books:
-    title = book.find("div", attrs={"class": "Epkrse tAO7hf"}).get_text()
+    # print(f'book: {book}')
+    title = book.find("div", attrs={"class": "Epkrse"}).get_text()
+    try:
+        org_price = book.find("span", attrs={"class": "SUZt4c P8AFK"}).get_text()
+    except AttributeError:
+        org_price = "NOT ON SALE"
     price = book.find("span", attrs={"class": "VfPpfd VixbEe"}).get_text()
-    print(f"Title: {title} | Price: {price}")
+
+    print(f"Title: {title} | Price: {price} | Original Price: {org_price}")
 
 with open("books.html", "w", encoding="utf8") as f:
     f.write(str(soup.prettify())) # html 문서 예쁘게 출력
