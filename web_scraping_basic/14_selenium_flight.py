2d45d387546bec95e21e67218a10c33839df9a67
이훈구 <foxworld@gmail.com>
2026-01-23 12:53:53 +0900

    web scraping study

--- web_scraping_basic/14_selenium_flight.py
+++ web_scraping_basic/14_selenium_flight.py
@@ -9,24 +9,45 @@ browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 browser.maximize_window() # 창 최대화
 
 def get_element(elems, target):
+    result = []
     for elem in elems:
-        if elem.find_element(By.TAG_NAME, "B").text.strip() == target:
-            break
-    return elem
+        try:
+            b = elem.find_element(By.TAG_NAME, "b")
+            if b.text.strip() == target:
+                result.append(elem)
+        except:
+            pass
+
+    print("찾은 개수:", len(result))
+    return result
+
 
 url = "https://flight.naver.com/flights/"
 browser.get(url)
+time.sleep(5)
+
 browser.find_element(By.CSS_SELECTOR, '[data-event-area-code="rnd.depdate1"]').click()
+
+elems = browser.find_elements(By.CSS_SELECTOR, ".sc-jlZhew.hTjJbq.inner")
+es = get_element(elems, "26")
+print(len(es), es)
+if len(es) > 0:
+    print(es[0].text, es[0].tag_name, es[0].get_attribute("innerHTML"))
+    es[0].click()
+
 time.sleep(5)
 
 elems = browser.find_elements(By.CSS_SELECTOR, ".sc-jlZhew.hTjJbq.inner")
-elem = get_element(elems, "26")
-print(elem.text)
-elem.click()
+es = get_element(elems, "3")
+print(len(es), es)
+if len(es) > 0:
+    print(es[1].text, es[1].tag_name, es[1].get_attribute("innerHTML"))
+    es[1].click()
+time.sleep(10)
 
-elems = browser.find_elements(By.CSS_SELECTOR, ".sc-jlZhew.hTjJbq.inner")
-elem = get_element(elems, "31")
-print(elem.text)
-elem.click()
+elems = browser.find_elements(By.CSS_SELECTOR, "img[alt='제주']")
+if len(elems) > 0:
+    for i in range(len(elems)):
+        print(elems[i].get_attribute("innerHTML"))
 
-time.sleep(100)
+time.sleep(30)
